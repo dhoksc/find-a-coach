dc641a4bf2b083b2e63179aab5d96682c409f657
Oleg Nesterov <olegnesterov@hotmail.com>
2021-10-14 19:29:49 +0300

    first checkpoint

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,23 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
 
-export default createStore({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+import coachesModule from "./modules/coaches/index.js";
+import requestsModule from "./modules/requests/index.js";
+
+const store = createStore({
   modules: {
-  }
-})
+    coaches: coachesModule,
+    requests: requestsModule,
+  },
+  state() {
+    return {
+      userId: "c3",
+    };
+  },
+  getters: {
+    userId(state) {
+      return state.userId;
+    },
+  },
+});
+
+export default store;
